13b9aeaf58156869dacf68be44ddfb86dc494cc7
FilipP130 <filip.padacz5@interia.pl>
2026-01-07 23:02:34 +0100

    1

---
+++ Recenzex/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Recenzex.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
