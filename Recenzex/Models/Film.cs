698ea901bcb6a877e3b8b57b02391f8c6f4005b6
FilipP130 <filip.padacz5@interia.pl>
2026-01-07 19:44:39 +0100

    Dodaj pliki projektów.

---
+++ Recenzex/Models/Film.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Recenzex.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tytuł jest wymagany")]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Range(1900, 2100, ErrorMessage = "Rok musi być pomiędzy 1900 a 2100")]
+        public int Year { get; set; }
+
+        [Required(ErrorMessage = "Opis jest wymagany")]
+        [StringLength(1000)]
+        public string Description { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+        public Genre? Genre { get; set; }
+
+        public ICollection<Review>? Reviews { get; set; }
+    }
+}
