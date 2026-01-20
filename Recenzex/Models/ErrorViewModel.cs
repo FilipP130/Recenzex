483f0593493efdb9e9263a7c79e03f337f431763
FilipP130 <filip.padacz5@interia.pl>
2026-01-20 18:54:23 +0100

    Delete Recenzex directory

--- Recenzex/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Recenzex.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
